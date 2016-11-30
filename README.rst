e9482289d18859de437bc644d7fec99d3d50bcfa
Tommy Yu <tommy.yu@auckland.ac.nz>
2016-11-30 17:17:21 +1300

    Initial boilerplate
    - Plus some initial information.

---
+++ README.rst
@@ -0,0 +1,33 @@
+nunja.serve
+===========
+
+A minimum implementation for serving |nunja|_ over HTTP.
+
+.. |nunja| replace:: ``nunja``
+.. |nunja.serve| replace:: ``nunja.serve``
+.. _nunja: https://pypi.python.org/pypi/nunja
+
+
+Installation
+------------
+
+Currently under development; please install |nunja| first by following
+its current installation instructions, before cloing this repository and
+running ``python setup.py develop``.
+
+
+Contribute
+----------
+
+- Issue Tracker: https://github.com/calmjs/nunja.serve/issues
+- Source Code: https://github.com/calmjs/nunja.serve
+
+
+Legal
+-----
+
+The |nunja.serve| package is part of the calmjs project.
+
+The calmjs project is copyright (c) 2016 Auckland Bioengineering
+Institute, University of Auckland.  |nunja.serve| is licensed under the
+terms of the GPLv2 or later.
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+version = '0.0'
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    open('CHANGES.rst').read()
+    + '\n')
+
+setup(
+    name='nunja.serve',
+    version=version,
+    description="Package to help serve nunja",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+    keywords='',
+    author='Tommy Yu',
+    author_email='tommy.yu@auckland.ac.nz',
+    url='https://github.com/calmjs/nunja.serve',
+    license='gpl',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['nunja'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+    ],
+    entry_points={
+    },
+)
